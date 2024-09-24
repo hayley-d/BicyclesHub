3771b4a56c26395832e68a24295e334ba99ae53c
Hayley Dodkins <hayleydod@proton.me>
2024-09-24 03:53:35 +0200

    Completed nav bar

---
+++ BicyclesHub/Controllers/Globals.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BicyclesHub.Controllers
+{
+    public static class Globals
+    {
+        public static string ConnectionString = "Data Source=HAYLEY-LAPTOP\\SQLEXPRESS;Initial Catalog=BikeStore;Integrated Security=True;";
+    }
+}
